41679fdfc599e97d04537077789933666429b1d2
BenjyC <ben.coler97@gmail.com>
2017-10-05 22:07:31 +0100

    twoSum Problem

---
+++ twoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums, target):
+        i = 0
+        while i < len(nums)-1:
+            j = i + 1
+            while j < len(nums):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                j += 1
+            i += 1
